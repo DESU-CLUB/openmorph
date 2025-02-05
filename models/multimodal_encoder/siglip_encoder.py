f4dc4bb9d7bb6ff6ab5c4eed14b165ee9126dcc0
DESU-CLUB <warrenlowkh@gmail.com>
2025-02-05 11:07:29 +0800

    Added basic encoder for SiGLIP

---
+++ models/multimodal_encoder/siglip_encoder.py
@@ -0,0 +1,39 @@
+import torch
+import torch.nn as nn
+
+from transformers import CLIPVisionModel, CLIPImageProcessor, CLIPVisionConfig
+
+
+class SiGLIPVisionTower(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
+        self.device_map = "auto"  # Set to auto for now, will set up args later
+        model_name = "google/siglip-base-patch16-256-i18n"
+        self.image_processor = CLIPImageProcessor.from_pretrained(
+            "google/siglip-base-patch16-256-i18n"
+        )
+        self.vision_tower = CLIPVisionModel.from_pretrained(
+            model_name, device_map=device_map
+        )
+        self.vision_tower.requires_grad_(False)  # Freeze encoder
+
+    @torch.no_grad()
+    def forward(self, images):
+        # Modified from LLaVA/llava/model/multimodal_encoder/clip_encoder.py
+        if type(images) is list:
+            image_feats = []
+            for image in images:
+                image_forward_out = self.vision_tower(
+                    image.to(device=self.device, dtype=torch.bfloat16).unsqueeze(0),
+                    output_hidden_states=True,
+                )
+                image_feats.append(
+                    image_forward_out
+                )  # TODO:Not sure what feature_select does in LLaVA,
+        else:
+            image_feats = self.vision_tower(
+                images.to(device=self.device, dtype=self.dtype),
+                output_hidden_states=True,
+            )
+        return image_feats
